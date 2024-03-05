ed69916d4a95a500f9e45d6e7016d97d53c569e7
Ryan Taite <ryan.taite@betterlifepartners.com>
2024-03-05 09:42:34 -0700

    Shift endpoint logic into it's own method
    Allows for better testing once it's made public

--- Example.Api/Endpoints/AttendanceEndpoints.cs
+++ Example.Api/Endpoints/AttendanceEndpoints.cs
@@ -11,32 +11,37 @@ public static class AttendanceEndpoints
     {
         app.MapPut("/attendance/toggle",
                 async (
-                    IValidator<UpdateAttendanceRequest> validator,
                     IMediator mediator,
+                    IValidator<UpdateAttendanceRequest> validator,
                     UpdateAttendanceRequest request
-                ) =>
-                {
-                    var validationResult = await validator.ValidateAsync(request);
-                    if (!validationResult.IsValid)
-                    {
-                        return Results.ValidationProblem(validationResult.ToDictionary());
-                    }
-
-                    var result = await mediator.Send(request);
-                    return result.StatusCode switch
-                    {
-                        HttpStatusCode.OK => Results.Ok(result.Payload),
-                        HttpStatusCode.NotFound => Results.NotFound(result.ErrorMessage),
-                        HttpStatusCode.BadRequest => Results.BadRequest(result.ErrorMessage),
-                        HttpStatusCode.InternalServerError => Results.Problem(detail: result.ErrorMessage),
-                        _ => Results.StatusCode((int)result.StatusCode)
-                    };
-                })
+                ) => await ToggleAttendance(mediator, validator, request))
             // These `Produces...` statements tell OpenApi (the Swagger page) what possible results the endpoint can return
             .Produces<UpdateAttendanceResult>()
             .Produces<string>(StatusCodes.Status404NotFound)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .ProducesProblem(StatusCodes.Status500InternalServerError)
             .ProducesValidationProblem();
+    }
+
+    private static async Task<IResult> ToggleAttendance(
+        ISender mediator,
+        IValidator<UpdateAttendanceRequest> validator, 
+        UpdateAttendanceRequest request) 
+    {
+        var validationResult = await validator.ValidateAsync(request);
+        if (!validationResult.IsValid)
+        {
+            return Results.ValidationProblem(validationResult.ToDictionary());
+        }
+
+        var result = await mediator.Send(request);
+        return result.StatusCode switch
+        {
+            HttpStatusCode.OK => Results.Ok(result.Payload),
+            HttpStatusCode.NotFound => Results.NotFound(result.ErrorMessage),
+            HttpStatusCode.BadRequest => Results.BadRequest(result.ErrorMessage),
+            HttpStatusCode.InternalServerError => Results.Problem(detail: result.ErrorMessage),
+            _ => Results.StatusCode((int)result.StatusCode)
+        };
     }
 }
